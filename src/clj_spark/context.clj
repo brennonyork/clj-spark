2a440c8d373971d2c07a8f820bd1f25143b7193c
Brennon York <brennon.york@gmail.com>
2014-07-11 17:20:04 -0400

    Added more to the context

--- src/clj_spark/context.clj
+++ src/clj_spark/context.clj
@@ -2,11 +2,8 @@
       Spark context objects"
       :author "Brennon York"}
   clj-spark.context
-  (:import [org.apache.spark.api.java JavaSparkContext]))
-
-(defmacro spark-open
-  "Example: (spark-open :hdfs"
-  [])
+  (:import [org.apache.spark.api.java JavaSparkContext])
+  (:require [clj-spark.util :as util]))
 
 (defmacro with-context
   "Defines a wrapper around a SparkContext to ensure it gets shut down
@@ -14,6 +11,33 @@
 
   Example: (with-context sc [\"local[2]\" \"my-app\"] ... )"
   [sym [& ctx-args] & body]
-  `(let [~(symbol sym) (JavaSparkContext. ~@ctx-args)]
+  `(let [~(symbol sym) (JavaSparkContext. ~@ctx-args)
+         start-time# (.startTime ~(symbol sym))]
      ~@body
-     (.stop ~(symbol sym))))
+     (.stop ~(symbol sym))
+     (- (System/currentTimeMillis) start-time#) 1000))
+
+(defmacro parallelize
+  ""
+  ([coll ctx]
+   `(.parallelize ~ctx (java.util.ArrayList. ~coll)))
+  ([coll numSlices ctx]
+   `(.parallelize ~ctx (java.util.ArrayList. ~coll) ~numSlices)))
+
+(defmacro parallelize->double
+  ""
+  ([coll ctx]
+   `(.parallelizeDoubles ~ctx (java.util.ArrayList. (map double ~coll))))
+  ([coll numSlices ctx]
+   `(.parallelizeDoubles ~ctx (java.util.ArrayList. (map double ~coll))
+                         ~numSlices)))
+
+(defmacro parallelize->pair
+  ""
+  ([coll ctx]
+   `(.parallelizePairs ~ctx (java.util.ArrayList.
+                             (map (fn [[x y]] (util/box-tuple2 x y)) ~coll))))
+  ([coll numSlices ctx]
+   `(.parallelizePairs ~ctx (java.util.ArrayList.
+                             (map (fn [[x y]] (util/box-tuple2 x y)) ~coll))
+                       ~numSlices)))
--- src/clj_spark/util.clj
+++ src/clj_spark/util.clj
@@ -7,3 +7,11 @@
 (defmacro unbox-tuple3
   [x]
   `(identity [(._1 ~x) (._2 ~x) (._3 ~x)]))
+
+(defmacro box-tuple2
+  [x y]
+  (scala.Tuple2. x y))
+
+(defmacro box-tuple3
+  [x y z]
+  (scala.Tuple3. x y z))
